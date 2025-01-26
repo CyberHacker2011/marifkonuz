b9bf1b22fd06bed2cac6dddf642a97ec0668c0c2
CyberHacker2011 <ibrohimshaymardanov011@outlook.com>
2025-01-26 15:04:47 +0500

    eighth commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,18 +5,18 @@ const AboutPage = () => {
   
   return (
     <>
-        <div className='grid lg:grid-rows-1 grid-rows-2 grid-cols-1 lg:grid-cols-2 p-5'>
-          <div className='flex flex-col gap-2 px-10 py-5 justify-center text-gray-900'>
-            <h1 className='text-3xl font-bold '>What is <q>Marifkon<sub>uz</sub></q> and who works there</h1>
-            <p className='font-sans text-lg'>
+        <div className='grid lg:grid-rows-1 grid-rows-2 grid-cols-1 lg:grid-cols-2 p-5 gap-3'>
+          <div className='flex flex-col gap-2 px-3 md:px-10 py-5  items-center justify-center text-start text-gray-900'>
+            <h1 className=' text-2xl md:text-3xl font-bold '>What is <q>Marifkon<sub>uz</sub></q> and who works there</h1>
+            <p className='font-sans text-base md:text-lg'>
               Our project is designed to provide educational resources, courses and informations on various topics. Here, you can find valuable knowledge ranging from basic shool subjects to additional extracurricular studies and beyond.
             </p>
             <div>
               
-            <p className='font-sans text-lg'>
+            <p className='font-sans text-base md:text-lg'>
             Our team contains school students from Uzbekistan. They are:
             </p>
-            <ul className='font-mono font-bold ml-2 text-lg'>
+            <ul className='font-mono font-bold ml-2 tet-sm md:text-lg'>
                 <li >
                   <Link className='hover:text-blue-400 inline-block' href={'https://t.me/i11tech'}>Ibrohim</Link>
                 </li>
