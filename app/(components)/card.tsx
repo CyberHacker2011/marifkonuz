659c9fb6a10c014d5d55170c6038a042322ea5f0
Ibrohim <ibrohimshaymardanov011@gmail.com>
2025-01-27 09:13:37 +0500

    tenth commit

--- app/(components)/card.tsx
+++ app/(components)/card.tsx
@@ -27,13 +27,37 @@ const Card = () => {
                 <span className='text-gray-600'>
                     Good boy with good experience on programming and owner of marifkonuz because of his good leadership skills.
                 </span>
-                <div className='flex items-center justify-center'>
-                    <Link href={'https://x.com'}>
+                <div className='flex items-center justify-center gap-2'>
+                    <Link href={'https://x.com'} className='opacity-80 hover:opacity-60'>
                         <Image
                         src={'/twitter.svg'}
                         alt='twitter logo'
-                        width={15}
-                        height={15}
+                        width={20}
+                        height={20}
+                        />
+                    </Link>
+                    <Link href={'https://t.me'} className='opacity-80 hover:opacity-60'>
+                        <Image
+                        src={'/telegram.svg'}
+                        alt='telegram logo'
+                        width={20}
+                        height={20}
+                        />
+                    </Link>
+                    <Link href={'https://linkedin.com'} className='opacity-80 hover:opacity-60'>
+                        <Image
+                        src={'/linkedin.svg'}
+                        alt='linkedin logo'
+                        width={20}
+                        height={20}
+                        />
+                    </Link>
+                    <Link href={'https://youtube.com'} className='opacity-80 hover:opacity-60'>
+                        <Image
+                        src={'/youtube.svg'}
+                        alt='youtube logo'
+                        width={20}
+                        height={20}
                         />
                     </Link>
                     
