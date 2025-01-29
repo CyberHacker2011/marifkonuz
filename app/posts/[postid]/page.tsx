37d008da35c53b4f3b6da0e9274a9447c6d0d309
Ibrohim <ibrohimshaymardanov011@gmail.com>
2025-01-29 21:23:39 +0500

    fixing bug in [postid]/page.tsx

--- app/posts/[postid]/page.tsx
+++ app/posts/[postid]/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from "react";
+import { useParams } from "next/navigation";
 import PostsList from "@/api/posts";
 
 type PostListType = {
@@ -8,28 +11,25 @@ type PostListType = {
   body: string;
 };
 
-type ParamsType = {
-  params: { postid: string };
-};
+const PostPage = () => {
+  const { postid } = useParams<{ postid: string }>();
 
-const PostPage = ({ params }: ParamsType) => {
-  const { postid } =  params; 
-  const post = PostsList.find((p: PostListType) => p.id === postid); 
+  const post = PostsList.find((p: PostListType) => p.id === postid);
 
   if (!post) {
     return <div>Post not found</div>;
   }
 
   return (
-    <>
-      <div className="flex justify-center items-center px-10 py-5">
-        <div className="flex flex-col shadow-lg justify-center items-center py-4 px-5 md:py-10 md:px-20 max-w-lg md:max-w-xl gap-2">
-          <h1 className="text-2xl font-bold">{post.title}</h1>
-          <p className="font-thin"><span className="font-bold">Author:</span> {post.author}</p>
-          <p>{post.body}</p>
-        </div>
+    <div className="flex justify-center items-center px-10 py-5">
+      <div className="flex flex-col shadow-lg justify-center items-center py-4 px-5 md:py-10 md:px-20 max-w-lg md:max-w-xl gap-2">
+        <h1 className="text-2xl font-bold">{post.title}</h1>
+        <p className="font-thin">
+          <span className="font-bold">Author:</span> {post.author}
+        </p>
+        <p>{post.body}</p>
       </div>
-    </>
+    </div>
   );
 };
 
